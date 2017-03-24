cfdd0db185da9a49136ca897cbe739ec8f8aa77b
nicPorcu <nicolo.porcu@gmail.com>
2017-03-24 10:42:12 -0700

    Add files via upload
    Nicolo Porcu added DemoSponge

---
+++ DemoSponge.java
@@ -0,0 +1,13 @@
+public class DemoSponge extends SeaSponge
+{
+    
+    public DemoSponge()
+    {
+        super("Demosponge","This is an actual animal");
+    }
+    public DemoSponge(String name, String description)
+    {
+        super(name,description);
+    }
+    
+}
