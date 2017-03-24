5129fe25d974d5949fad4dfcae9a520dad352359
498480689 <sw498480689@gmail.com>
2017-03-24 10:45:31 -0700

    Add files via upload
    RedPanda

---
+++ RedPanda.java
@@ -0,0 +1,38 @@
+public class RedPanda extends Animal implements Walking 
+{
+    
+    public RedPanda()
+    {
+        super("Red Panda", "Panda de color rojo");
+        
+        
+        
+    }
+    
+    
+    public String eat()
+    {
+        return "Red Panda eats Bambooooooooooooo";
+        
+        
+    }
+    
+    public String play(){
+        return "Red Panda plays with Bamboooooooooooo";
+    }
+    
+    public String walk()
+    {
+        return "Red Panda walks with a Bamboooooooooooo";
+    }
+    
+    public String sleep()
+    {
+        return "Red Panda dream of Bamboooooooooooo";
+    }
+
+        
+    
+    
+}
+    
