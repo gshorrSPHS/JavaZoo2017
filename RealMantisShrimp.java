c05e71899a09b8a235219082300ffb0ae0594efd
zoekaplan <zoerkaplann@gmail.com>
2017-03-24 10:34:58 -0700

    Zoe added RealMantisShrimp

---
+++ RealMantisShrimp.java
@@ -0,0 +1,37 @@
+public class RealMantisShrimp extends MantisShrimp
+{
+    public RealMantisShrimp()
+    {
+        super();
+    }
+    
+    public String eat()
+    {
+        return super.eat();
+    }
+    
+    public String play()
+    {
+        return super.play();
+    }
+    
+    public String makeNoise()
+    {
+        return super.makeNoise();
+    }
+    
+    public String walk()
+    {
+        return super.walk();  
+    }
+    
+    public String swim()
+    {
+        return super.swim();
+    }
+    
+    public String sillyHuman()
+    {
+        return "Silly Human, I never made a fake mantis shrimp.";
+    }
+}
